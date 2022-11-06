a88a59107c81564634abc9fe646c576cc2da1312
Felipe Vieira <flp.vieira92@gmail.com>
2022-11-05 22:26:44 -0300

    Commit Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { NovaTransferencia } from './nova-transferencia/nova-transferencia.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,7 +6,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTransferencia
   ],
   imports: [
     BrowserModule
