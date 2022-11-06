b1ee211113b0a0695bd1040060d94b2ce60bcf49
Felipe Vieira <flp.vieira92@gmail.com>
2022-11-06 01:10:15 -0300

    EventEmitter

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -8,13 +8,21 @@ import { Component, EventEmitter, Output } from "@angular/core";
 export class NovaTransferencia{
 
   @Output() aoTransferir = new EventEmitter<any>();
-  valor: number = 1;
-  quantidade: number = 1;
+  valor: number = 0;
+  quantidade: number = 0;
 
 
   transferir(){
     console.log("clicou")
+
     const valorEmitir = {valor: this.valor, quantidade: this.quantidade}
     this.aoTransferir.emit(valorEmitir)
+
+    this.limpar();
+  }
+
+  limpar(){
+    this.valor = 0;
+    this.quantidade = 0;
   }
 }
